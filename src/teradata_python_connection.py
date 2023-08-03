b507552aa8d123759de4d5f43b2239f3e0a29d0f
krutik-2-11 <krutik.pathak@teradata.com>
2023-08-02 17:59:06 -0700

    python code updated

--- src/teradata_python_connection.py
+++ src/teradata_python_connection.py
@@ -3,22 +3,38 @@ import teradatasql
 DB_URL = ""                                 #Add Host
 USER = ""                                   #Add Username
 PASS = ""                                   #Add Password
-QUERY = "SELECT * FROM dbc.dbcinfo"         #Sample query
 
 try:
     # Establish a connection to the Teradata database
     with teradatasql.connect(host=DB_URL, user=USER, password=PASS) as con:
         # Create a cursor to execute queries
         with con.cursor() as cur:
-            print("Final contents of table")
-
             try:
-                # Execute the query
-                cur.execute(QUERY)
+                # Creating the table
+                cur.execute (f"CREATE SET TABLE {USER}.SampleEmployee \
+                            (Associate_Id     INTEGER, \
+                            Associate_Name   CHAR(25), \
+                            Job_Title        VARCHAR(25)) \
+                            UNIQUE PRIMARY INDEX (Associate_Id);")
+                
+                print(f"Sample table {USER}.SampleEmployee created.")
+
+                # Adding sample data into SampleEmployee table
+                cur.execute (f"INSERT INTO {USER}.SampleEmployee VALUES (1, 'Richard Hendricks','CEO')")
+                cur.execute (f"INSERT INTO {USER}.SampleEmployee VALUES (2, 'Jared Dunn','CFO')")
+                cur.execute (f"INSERT INTO {USER}.SampleEmployee VALUES (3, 'Jian Yang','Intern')")
+
+                print("Sample data added to table demo_user.SampleEmployee.")
+
+                # Execute the SELECT query to get the results from DBC.SampleEmployee table 
+                cur.execute(f"SELECT * FROM {USER}.SampleEmployee")
 
                 # Extract data from the result set and print it
                 for row in cur:
-                    print(f"setting: {row[0]}, value: {row[1]}")
+                    print(f"Associate ID: {row[0]}, Associate_Name: {row[1]}, Job_Title:{row[2]}")
+
+                
+
             except teradatasql.DatabaseError as db_err:
                 # Handle any errors that occur during query execution
                 print("Error while executing the query:", db_err)
